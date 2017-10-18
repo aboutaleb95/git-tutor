06daf4eff8d59363a2bffaa8631ee617781310e4
Mahmoud Aboutaleb <ma.aboutaleb@yahoo.com>
2017-10-17 19:16:50 -0700

    made changes

---
+++ runy.rb
@@ -0,0 +1,13 @@
+some_var = "false"
+another_var = "nil"
+if some_var == "pink elephant"
+	puts "dont shit every wear"
+elsif another_var.nil?
+	puts "question mark tarbtaiit"
+elsif some_var == false
+	puts "looks like this one should work"
+else 
+puts "i gues eno ebn metnaka"
+end
+		
+		
